c5707405fac903ddf577045eb11ec9090fa1b897
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2024-02-01 20:41:24 +0530

    first commit

---
+++ server/connection/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const DB = process.env.MONGO_URI
+
+mongoose.connect(DB,{})
+.then(()=>{
+    console.log("Mongodb connected");
+})
+.catch((err)=>{
+    console.log("Error to connect database" + err)
+})
