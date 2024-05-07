7280b4f59097fc4a1efb5976c910e6f7ab328827
Niladri Sekhar Rout <niladri30012003@gmail.com>
2024-05-07 23:54:42 +0530

    changes to profile page

--- client/src/afterlogin/Profile.js
+++ client/src/afterlogin/Profile.js
@@ -1,7 +1,8 @@
 import React,{useEffect, useState} from 'react'
 import Afterloginusernav from '../navbar/Afterloginusernav'
 import Foot from "../footer/Foot";
-import axios from "axios"
+import axios from "axios";
+import "../customcss/profile.css"
 const Profile = () => {
     const [profilePic, setProfilePic] = useState('');
     const [email,setEmail] = useState("")
@@ -51,12 +52,25 @@ const Profile = () => {
     <div className='profile'>
 
         <Afterloginusernav/>
-        <div style={{height:'50%',width:'50%',display:'flex',justifyContent:'center',alignItems:'center'}}>
+       {/* <div className='afterprofile' style={{height:'50%',width:'50%',display:'flex',justifyContent:'center',alignItems:'center'}}>
         <div style={{marginTop:'10rem',display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',height:'50%',width:'50%'}}>
             <img src={profilePic} alt="" style={{borderRadius:'50%', height:'200px', width:'200px',display:'flex',justifyContent:'center',alignItems:'center',marginLeft:'600px'}}/>
             <button onClick={editprofile} style={{marginLeft:'600px' , width:'100px',display:'flex',flexDirection:"row"}}>Edit Profile Picture</button>
             <button onClick={editprofile} style={{marginLeft:'600px', width:'100px',display:'flex',flexDirection:'row'}}>Edit Username</button>
             <button onClick={editprofile} style={{marginLeft:'600px',width:'100px',display:'flex',flexDirection:'row'}}>Edit Password</button>
+        </div>
+       </div>*/}
+
+
+       <div className='afterprofile' >
+        <div >
+            <img src={profilePic} alt="" className='imageProfile'/>
+            <div className='profileButton'>
+            <button onClick={editprofile}>Edit Profile Picture</button>
+            <button onClick={editprofile}>Edit Username</button>
+            <button onClick={editprofile}>Edit Password</button>
+            </div>
+            
         </div>
         </div>
         <Foot/>
